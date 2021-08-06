1123e6c03947fbb3d68cb80a8d650729893864ad
nandhinimano11@gmail.com <nandhinimano11@gmail.com>
2021-08-06 08:06:58 +0530

    deployment

---
+++ build/precache-manifest.46153f3950d41f036e2ccb4c7404aa0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "55081a8cec4dcf9b5c08",
+    "url": "/static/css/main.c380df66.chunk.css"
+  },
+  {
+    "revision": "55081a8cec4dcf9b5c08",
+    "url": "/static/js/main.55081a8c.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "402d458c68c36b611069",
+    "url": "/static/css/2.666842a2.chunk.css"
+  },
+  {
+    "revision": "402d458c68c36b611069",
+    "url": "/static/js/2.402d458c.chunk.js"
+  },
+  {
+    "revision": "6c169700838580dd5dc0bc396c3a4267",
+    "url": "/static/media/23.6c169700.png"
+  },
+  {
+    "revision": "3129158302bbb6a9f93acc17c585c8f3",
+    "url": "/static/media/uplod_icon.31291583.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "cbb137fbee4b346ae40337c4a1091f81",
+    "url": "/index.html"
+  }
+];
